bcac616f00ae7c2b58a0d67e36cba76fcb0eb470
IshuAgrawal11 <ishuagrawal1103@gmail.com>
2026-02-28 18:31:03 +0000

    second

--- Terraform-for-DevOps/variable.tf
+++ Terraform-for-DevOps/variable.tf
@@ -21,7 +21,13 @@ variable "server_config" {
     "db-server" = {
       ami           = "ami-073130f74f5ffb161" # Ubuntu
       instance_type = "t3.small"
-      storage_size  = 20
+      storage_size  = 10
+      instance_state = "running"
+    }
+    "test-server" = {
+      ami           = "ami-073130f74f5ffb161" # Ubuntu
+      instance_type = "t3.micro"
+      storage_size  = 10
       instance_state = "running"
     }
   }
